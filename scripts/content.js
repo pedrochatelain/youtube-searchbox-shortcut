13d7f5432ce221a6e4556cad4ff5a8b30acbc6ea
pedrochatelain <pedrochatelain@gmail.com>
2024-04-17 01:05:42 -0300

    check uppercase y, theater and fullscreen mode

--- scripts/content.js
+++ scripts/content.js
@@ -1,12 +1,20 @@
 const searchBar = document.querySelector('#search-input');
 
+
 if (searchBar) {
   
   document.addEventListener("keydown", (event) => {
     const keyName = event.key;
-    if (keyName === 'y') {
+    if (keyName === 'y' || keyName === 'Y') {
       event.preventDefault();
-      searchBar.firstChild.focus();
+      const fullscreenButton = document.querySelector('.ytp-fullscreen-button.ytp-button');
+      console.log(fullscreenButton.getAttribute('data-title-no-tooltip'))
+      if (fullscreenButton && fullscreenButton.getAttribute('data-title-no-tooltip') === "Full screen") {
+        searchBar.firstChild.focus();
+      }
+      if (! fullscreenButton) {
+        searchBar.firstChild.focus();
+      }
     }
   
     searchBar.addEventListener("keydown", (event) => event.stopPropagation() )
