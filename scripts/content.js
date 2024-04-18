df1a31e54e663543ffd67fa1515d53f55c130bc1
pedrochatelain <pedrochatelain@gmail.com>
2024-04-18 19:30:14 -0300

    wrap code in init function

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,26 @@
-const searchBar = document.querySelector('#search-input');
 
-
-if (searchBar) {
+function init() {
+  const searchBar = document.querySelector('#search-input');
   
-  document.addEventListener("keydown", (event) => {
-    const keyName = event.key;
-    if (keyName === 'y' || keyName === 'Y') {
-      event.preventDefault();
-      const fullscreenButton = document.querySelector('.ytp-fullscreen-button.ytp-button');
-      console.log(fullscreenButton.getAttribute('data-title-no-tooltip'))
-      if (fullscreenButton && fullscreenButton.getAttribute('data-title-no-tooltip') === "Full screen") {
-        searchBar.firstChild.focus();
-      }
-      if (! fullscreenButton) {
-        searchBar.firstChild.focus();
-      }
-    }
   
-    searchBar.addEventListener("keydown", (event) => event.stopPropagation() )
-  });
+  if (searchBar) {
+  
+    document.addEventListener("keydown", (event) => {
+      const keyName = event.key;
+      if (keyName === 'y' || keyName === 'Y') {
+        event.preventDefault();
+        const fullscreenButton = document.querySelector('.ytp-fullscreen-button.ytp-button');
+        if (fullscreenButton && fullscreenButton.getAttribute('data-title-no-tooltip') === "Full screen") {
+          searchBar.firstChild.focus();
+        }
+        if (! fullscreenButton) {
+          searchBar.firstChild.focus();
+        }
+      }
+    
+      searchBar.addEventListener("keydown", (event) => event.stopPropagation() )
+    });
+  }
 }
+
+document.addEventListener("DOMContentLoaded", init());
